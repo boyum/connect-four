8367f375f85ecf4ee5cbe266b9cb2c3f4e0c5ebd
Sindre Bøyum <sindre.boyum@gmail.com>
2020-10-31 11:01:08 +0100

    Get a random index based on the number of columns

--- api/next-move.go
+++ api/next-move.go
@@ -1,11 +1,32 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
 )
 
+type Disc struct {
+	user uint
+}
+
+type Column struct {
+	discs []Disc
+}
+
+func getJson(r *http.Request, target interface{}) error {
+	defer r.Body.Close()
+
+	return json.NewDecoder(r.Body).Decode(target)
+}
+
 // Handler Exported http handler
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "{\"index\": 0}")
+	columns := []Column{}
+	getJson(r, columns)
+
+	index := rand.Intn(len(columns))
+
+	fmt.Fprintf(w, "{\"index\": %d}", index)
 }
