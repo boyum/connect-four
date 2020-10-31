81b00b0e66ba44387965813352f32ae4933218e7
Sindre Bøyum <sindre.boyum@gmail.com>
2020-10-31 10:19:20 +0100

    Fix lint

--- api/next-move.go
+++ api/next-move.go
@@ -1,10 +1,11 @@
 package handler
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
+// Handler Exported http handler
 func Handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "{\"index\": 0}")
 }
